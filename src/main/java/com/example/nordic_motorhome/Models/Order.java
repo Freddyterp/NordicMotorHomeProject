fee1c4ef1cfea80b15f4ecb0afae45fc314d7c87
Terpinator <frederik.terp1@gmail.com>
2020-05-26 12:20:03 +0200

    added order repositories

---
+++ src/main/java/com/example/nordic_motorhome/Models/Order.java
@@ -0,0 +1,85 @@
+package com.example.nordic_motorhome.Models;
+
+import java.util.Date;
+
+public class Order {
+        private int id;
+        private float price;
+        private Date startDate;
+        private Date endDate;
+        private Date reservationDate;
+        private Car reservedCar;
+        private Customer customer;
+
+    public Order(int id, float price, Date startDate, Date endDate, Date reservationDate, Car car, Customer customer) {
+        this.id = id;
+        this.price = price;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.reservationDate = reservationDate;
+        this.reservedCar = car;
+        this.customer = customer;
+    }
+
+    public Order() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getReservationDate(){return this.reservationDate;}
+
+    public java.sql.Date getDateAsSQL(java.util.Date Date) {
+        java.sql.Date sqlDate= new java.sql.Date(Date.getTime());
+        return sqlDate;
+    }
+
+    public void setReservationDate(Date reservationDate){
+        this.reservationDate = reservationDate;
+    }
+
+    public Car getReservedCar() {
+        return reservedCar;
+    }
+
+    public void setReservedCar(Car reservedCar) {
+        this.reservedCar = reservedCar;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
